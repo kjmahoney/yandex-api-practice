b92fdf8e6246abf9b74ccfa8fae50ad50ae40904
Kevin Mahoney <kjmahoney13@gmail.com>
2016-12-24 12:25:58 -0500

    working

--- js/app.js
+++ js/app.js
@@ -9,31 +9,33 @@
 
 $("#button").on("click", () => {
 
+
+  if ($("select").val() == "English"){
+    languageConversion = "&lang=en-zh"
+  }else{
+    languageConversion = "&lang=zh-en"
+  }
+
+  console.log($("select").val())
   const originalWord =$("#originalWordBox").val()
+
+
 
   const APIUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trns"+
                  "l.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text="
                  +originalWord+
-                 "&lang=en-zh"
+                 languageConversion
                 console.log(APIUrl)
 
-  // Make sure to add your API key to the URL!
   $.ajax({
     url: APIUrl,
     type: "get",
     dataType: "json"
-    // $.ajax takes an object as an argument with at least three key-value pairs...
-    // (1) The URL endpoint for the JSON object.
-    // (2) Type of HTTP request.
-    // (3) Datatype. Usually JSON.
   }).done((response) => {
-    console.log(response.text)
-    $("#translationWordBox").text(response.text)
-
-
+    $("#translationWordBox").val(response.text)
   }).fail(() => {
-    console.log("Ajax request fails!")
+    console.log("Ajax request failed!")
   }).always(() => {
-    console.log("AJAX working")
+    console.log("Ajax working")
   })
 })
