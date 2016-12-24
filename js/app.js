37099e93bb5a17c926448ce614bef413381b6b03
Kevin Mahoney <kjmahoney13@gmail.com>
2016-12-24 12:27:59 -0500

    tidy up code

--- js/app.js
+++ js/app.js
@@ -1,32 +1,25 @@
-// trnsl.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897
-//
-// https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text=hello&lang=en-ru
-//
-// 　chinese to english
 // https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text=开&lang=zh-en
 
 
 
 $("#button").on("click", () => {
 
-
+//look for English to Chinese and vice versa
   if ($("select").val() == "English"){
     languageConversion = "&lang=en-zh"
   }else{
     languageConversion = "&lang=zh-en"
   }
 
-  console.log($("select").val())
+//Find word to be Translated
   const originalWord =$("#originalWordBox").val()
 
-
-
+//Define URL to be called to Yandex
   const APIUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trns"+
                  "l.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text="
                  +originalWord+
                  languageConversion
-                console.log(APIUrl)
-
+//Make call to Yandex
   $.ajax({
     url: APIUrl,
     type: "get",
