165df34526cd35ddf32202dea22653c2040dc9e2
Kevin Mahoney <kjmahoney13@gmail.com>
2016-12-24 12:03:56 -0500

    added output button

--- js/app.js
+++ js/app.js
@@ -5,11 +5,21 @@
 // 　chinese to english
 // https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text=开&lang=zh-en
 
-$("button").on("click", () => {
+
+
+$("#button").on("click", () => {
+
+  const originalWord =$("#originalWordBox").val()
+
+  const APIUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trns"+
+                 "l.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text="
+                 +originalWord+
+                 "&lang=en-zh"
+                console.log(APIUrl)
+
   // Make sure to add your API key to the URL!
-  var url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text=friend&lang=en-zh"
   $.ajax({
-    url: url,
+    url: APIUrl,
     type: "get",
     dataType: "json"
     // $.ajax takes an object as an argument with at least three key-value pairs...
@@ -17,10 +27,13 @@ $("button").on("click", () => {
     // (2) Type of HTTP request.
     // (3) Datatype. Usually JSON.
   }).done((response) => {
-    console.log(console.log(response.text))
+    console.log(response.text)
+    $("#translationWordBox").text(response.text)
+
+
   }).fail(() => {
     console.log("Ajax request fails!")
   }).always(() => {
-    console.log("This always happens regardless of successful ajax request or not.")
+    console.log("AJAX working")
   })
 })
