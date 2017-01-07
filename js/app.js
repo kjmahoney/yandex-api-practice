0c73805c663b7b6e572e77f386702826a6b13e4c
Kevin Mahoney <kjmahoney13@gmail.com>
2017-01-07 16:09:00 -0500

    add support for korean

--- js/app.js
+++ js/app.js
@@ -1,35 +1,24 @@
-// https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text=开&lang=zh-en
-
-
 $("#originalWordBox, select").change(() => {
 
-let fromLang = $("#conversionBoxFrom").val()
-let toLang = $("#conversionBoxTo").val()
-//look for English to Chinese and vice versa
-  // if ($("#conversionBox").val() == "English"){
-  //   languageConversion = "en-zh"
-  // }else{
-  //   languageConversion = "zh-en"
-  // }
+  //select language to translate from
+  let fromLang = $("#conversionBoxFrom").val()
 
+  //select langauge to translate to
+  let toLang = $("#conversionBoxTo").val()
 
-
-//and a toLang
-
-
-//Find word to be Translated
+  //Find word to be Translated
   const originalWord =$("#originalWordBox").val()
 
-//Define URL to be called to Yandex
+  //Define URL to be called to Yandex
   const APIUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trns"+
-                 "l.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text="
-                 +originalWord
-                 +"&lang="
-                 +fromLang
-                 +"-"
-                 +toLang
-                 console.log(APIUrl)
-//Make call to Yandex
+  "l.1.1.20161224T005353Z.9e104eecc7c8560f.938a2af78ac0f6868c443d729e48ee590e87d897&text="
+  +originalWord
+  +"&lang="
+  +fromLang
+  +"-"
+  +toLang
+
+  //Make call to Yandex
   $.ajax({
     url: APIUrl,
     type: "get",
